88b57f30f70c5f998273ca17d7b00b85287bc858
Son Tran <sontran@berkeley.edu>
2019-05-26 11:08:10 -0700

    Solve equation a^3 + b^3 = c^3 + d^3

---
+++ cpp/cubeequation.cpp
@@ -0,0 +1,25 @@
+/*
+ * Listing all the solution for the equation: a^3 + b^3 = c^3 + d^3
+ * Where a, b, c, d is in the range from 0 to 1000
+ */
+#include <iostream>
+#include <unordered_map>
+#include <list>
+
+int main() {
+    int max = 10;
+    std::unordered_map<int, std::list<std::pair<int, int>>> map;
+    for (int a = 0; a <= max; a++) {
+        for (int b = 0; b <= max; b++) {
+            map[a*a*a + b*b*b].push_back(std::pair<int, int>(a, b));
+        }
+    }
+    for (auto& kv : map) {
+        for (auto& p1 : kv.second) {
+            for (auto& p2 : kv.second) {
+                std::cout << p1.first << " " << p1.second << " "
+                          << p2.first << " " << p2.second << std::endl;
+            }
+        }
+    }
+}
